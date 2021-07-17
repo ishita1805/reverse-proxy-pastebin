15efbd8f1f673e0b4c3c64f28a13abe9fed1b261
Ishita Kabra <ishitakabra1805@gmail.com>
2021-07-17 19:00:34 +0530

    health route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 5555;
+const port = process.env.PORT ;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
